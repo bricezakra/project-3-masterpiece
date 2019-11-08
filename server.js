cf9dc28379c29b65a1417ef790e1c68ce04de1b4
Randall <rfields1128@yahoo.com>
2019-11-08 14:20:47 -0600

    working on chatbox

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require("express");
 
-const mongoose = require("mongoose");
+const mongo = require('mongodb').MongoClient;
 const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
+const client = require('socket.io').listen(4000).sockets;
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
@@ -16,7 +17,66 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
+// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
+mongo.connect('mongodb://127.0.0.1/mongochat', function(err, db){
+    if(err){
+        throw err;
+    }
+
+    console.log('MongoDB connected...');
+
+    // Connect to Socket.io
+    client.on('connection', function(socket){
+        let chat = db.collection('chats');
+
+        // Create function to send status
+        sendStatus = function(s){
+            socket.emit('status', s);
+        }
+
+        // Get chats from mongo collection
+        chat.find().limit(100).sort({_id:1}).toArray(function(err, res){
+            if(err){
+                throw err;
+            }
+
+            // Emit the messages
+            socket.emit('output', res);
+        });
+
+        // Handle input events
+        socket.on('input', function(data){
+            let name = data.name;
+            let message = data.message;
+
+            // Check for name and message
+            if(name == '' || message == ''){
+                // Send error status
+                sendStatus('Please enter a name and message');
+            } else {
+                // Insert message
+                chat.insert({name: name, message: message}, function(){
+                    client.emit('output', [data]);
+
+                    // Send status object
+                    sendStatus({
+                        message: 'Message sent',
+                        clear: true
+                    });
+                });
+            }
+        });
+
+        // Handle clear
+        socket.on('clear', function(data){
+            // Remove all chats from collection
+            chat.remove({}, function(){
+                // Emit cleared
+                socket.emit('cleared');
+            });
+        });
+    });
+});
 
 // Start the API server
 app.listen(PORT, function() {
