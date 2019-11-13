7dfce410229e3e99843ffbee08659a0891af6e38
Randall <rfields1128@yahoo.com>
2019-11-13 13:03:07 -0600

    Added side navbar, cleaned up boilerplate code

--- routes/api/mapboxAPI.js
+++
@@ -1,8 +0,0 @@
-const router = require("express").Router();
-const mapboxController = require("../../controllers/booksController");
-
-// Matches with "/api/books"
-router.route("/")
-    .get(mapboxController.findKey)
-
-module.exports = router;
---
+++ src/components/SideNavigation.js
@@ -0,0 +1,88 @@
+import React from 'react';
+// import { Link } from 'react-router-dom'
+import SideNav, { NavItem, NavIcon, NavText } from '@trendmicro/react-sidenav';
+import {BrowserRouter as Router, Route } from 'react-router-dom';
+import About from './About';
+import Shop from './Shop';
+import SearchableMap from './SearchableMap';
+// Be sure to include styles at some point, probably during your bootstraping
+import '@trendmicro/react-sidenav/dist/react-sidenav.css';
+
+
+function SideNavigation() {
+    return (
+        <Router>
+            <Route render={({ location, history }) => (
+                <React.Fragment>
+                    <SideNav
+                        onSelect={(selected) => {
+                            const to = '/' + selected;
+                            if (location.pathname !== to) {
+                                history.push(to);
+                            }
+                        }}
+                    >
+                        <SideNav.Toggle />
+                        <SideNav.Nav defaultSelected="home">
+                            
+                            <NavItem eventKey="about">
+                                <NavIcon>
+                                    <i className="fa fa-fw fa-home" style={{ fontSize: '1.75em' }} />
+                                </NavIcon>
+                                <NavText>
+                                    About
+                        </NavText>
+                            </NavItem>
+                            <NavItem eventKey="shop">
+                                <NavIcon>
+                                    <i className="fa fa-fw fa-device" style={{ fontSize: '1.75em' }} />
+                                </NavIcon>
+                                <NavText>
+                                    Shop
+                        </NavText>
+                            </NavItem>
+                            <NavItem eventKey="map">
+                                <NavIcon>
+                                    <i className="fa fa-fw fa-home" style={{ fontSize: '1.75em' }} />
+                                </NavIcon>
+                                <NavText>
+                                   Wifi Map
+                        </NavText>
+                            </NavItem>
+                        </SideNav.Nav>
+                    </SideNav>
+                    <main>
+                        <Route path="/" exact component={props => <Home />} />
+                        <Route path="/about" component={props => <About />} />
+                        <Route path="/shop" component={props => <Shop />} />
+                        <Route path="/map" component={props => <SearchableMap />} />
+                    </main>
+                </React.Fragment>
+            )}
+            />
+        </Router>
+
+        // <nav>
+        //     <h3> navbar </h3>
+        //     <ul>
+        //         <Link to='/about'>
+        //             <li>About</li>
+        //         </Link>
+        //         <Link to='/map'>
+        //             <li>Map</li>
+        //         </Link>
+        //         <Link to='/shop'>
+        //             <li>Shop</li>
+        //         </Link>
+        //     </ul>
+        // </nav>
+    )
+}
+
+const Home = () => (
+    <div>
+      <h1> Home Page</h1>
+    </div>
+  )
+
+export default SideNavigation
