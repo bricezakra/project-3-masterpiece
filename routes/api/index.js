5381cf31a125cde49354e07a199515308ce5bb23
Randall <rfields1128@yahoo.com>
2019-11-13 14:37:29 -0600

    cleand up bits working with node express server

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,10 +1,8 @@
 const router = require("express").Router();
-const bookRoutes = require("./books");
 const mapboxRoutes = require("./mapboxAPI")
 
 
-// Book routes
-router.use("/books", bookRoutes);
+
 router.use("/mapbox", mapboxRoutes);
 
 module.exports = router;
