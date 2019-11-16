bfb279aff599990a1aca3142d303c6165340bcc0
makhsumbek karamatov <uzbekhabib23@gmail.com>
2019-11-16 00:09:59 -0600

    workin on files

--- client/src/components/AdvenTour.js
+++ client/src/components/AdvenTour.js
@@ -70,6 +70,16 @@ function AdvenTour () {
                <div> AdvenTour is Travel tour, that let's you live life with no excuses, and travel with no regret.
                     
                </div>
+               <ul>
+                   <li>Sometimes you will never know the value of a moment until it becomes a memory</li>
+
+               </ul>
+               <ul>
+                   <li>
+                       
+                   </li>
+               </ul>
+               
 
             </ul>
             </div>
