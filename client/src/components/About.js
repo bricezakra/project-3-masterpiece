664b71dc034ec0c773d6334b186d98b65a15d4f6
Randall <rfields1128@yahoo.com>
2019-11-17 13:40:17 -0600

    stash

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -1,20 +1,19 @@
 import React from 'react';
 
-
 function About() {
    return (
-       <div  style={{ marginLeft: 64 }}>
-           <h2><font color="blue"><strong>ABOUT OUR APP</strong></font></h2>
-           <h3> About Our App:</h3>
+      <div style={{ marginLeft: 64 }}>
+         <h2><font color="blue"><strong>ABOUT OUR APP</strong></font></h2>
+         <h3> About Our App:</h3>
 
-            <p>
-               Wi-Finder is an app that locates all free wifi locations in the 
-               city of New York, and the app is currently in progress of becoming nationwide. 
-               Our backend team which includes Brice Zakra and Randall Fields added features that 
-               makes each wifi location interactive. It will show the location name and address, and 
-               whether it is an outdoor kiosk, subway station, etc.
+         <p>
+            Wi-Finder is an app that locates all free wifi locations in the
+            city of New York, and the app is currently in progress of becoming nationwide.
+            Our backend team which includes Brice Zakra and Randall Fields added features that
+            makes each wifi location interactive. It will show the location name and address, and
+            whether it is an outdoor kiosk, subway station, etc.
             </p>
-        </div>
+      </div>
    )
 }
 
