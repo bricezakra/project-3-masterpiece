e568e0fb2017c6fd39d52a0febb4cf972c2afd09
makhsumbek karamatov <uzbekhabib23@gmail.com>
2019-11-17 16:27:14 -0600

    working on contact page

--- client/src/components/Contacts.js
+++ client/src/components/Contacts.js
@@ -21,7 +21,7 @@ function Contacts() {
           <Form.Control type="email" placeholder="Enter email" />
           <Form.Text className="text-muted">
             We'll never share your email with anyone else.
-      </Form.Text>
+          </Form.Text>
         </Form.Group>
 
         <Form.Group>
